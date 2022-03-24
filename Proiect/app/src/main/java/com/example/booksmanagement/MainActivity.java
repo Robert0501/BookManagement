575c221914db3eabb4afce8d82bbdf5513e1e833
Robert0501 <robert_amza@yahoo.com>
2022-03-24 08:38:40 +0200

    Clickable image

--- Proiect/app/src/main/java/com/example/booksmanagement/MainActivity.java
+++ Proiect/app/src/main/java/com/example/booksmanagement/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.booksmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    public void goToLibrary(View v){
+        setContentView(R.layout.library);
+    }
+
+    public void goToAddBooks(View v){
+        setContentView(R.layout.add_books);
+    }
+
+    public void goToBookInfo(View v){
+        setContentView(R.layout.book_info);
+    }
+
+    public void goToBorrowTo(View v){
+        setContentView(R.layout.borrow_to);
+    }
+
+    public void goToHome(View v){
         setContentView(R.layout.home);
     }
 }
